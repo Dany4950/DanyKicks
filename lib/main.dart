2850a79794dd2fb7082cf8024b3d3d188f455bcb
P.Sujay Daniel <p.sujaydaniel@PSujays-MacBook-Air.local>
2024-01-22 11:07:23 +0530

    this is the basic version of the Ecommerce Application

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Ecommerce());
+  
+}
+
+class Ecommerce extends StatelessWidget {
+  const Ecommerce({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
